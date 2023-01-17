6f5f78fc04b8e862b2d768006c012e9a74e8b8b9
k7shravan <66732539+shravan293@users.noreply.github.com>
2023-01-17 22:53:30 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -15,7 +15,7 @@ public class WelcomeRestController {
 		logger.info("***** WelcomeRestController::Constructor *****");
 	}
 
-	@GetMapping
+	@GetMapping(value="/welcome/{name}")
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
 		String msg = "Welcome to Ashok IT..!!";
