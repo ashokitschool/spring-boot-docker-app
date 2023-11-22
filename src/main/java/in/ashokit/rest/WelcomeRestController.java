656b6efe29124872edfaa89884a0265843a1894d
Ashok IT <30829130+ashokitschool@users.noreply.github.com>
2023-11-22 10:19:14 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -18,7 +18,7 @@ public class WelcomeRestController {
 	@GetMapping
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to Ashok IT - Software Training Institute..!!";
+		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
 		logger.info("***** welcomeMsg() execution end *****");
 		return msg;
 	}
