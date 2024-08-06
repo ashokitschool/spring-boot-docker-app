9760825f0d3e8f0d5ab99eb93e123f35c941d37c
Ashok <ashokitschool@gmail.com>
2024-08-06 07:56:30 +0530

    method changed

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -20,6 +20,7 @@ public class WelcomeRestController {
 		logger.info("***** welcomeMsg() execution start *****");
 		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
 		logger.info("***** welcomeMsg() execution end *****");
+		String s = "hello";
 		return msg;
 	}
 	
